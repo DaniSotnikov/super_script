c2176603d2595d15fc710611e6de6d8457339640
Danila Sotnikov <danisotnikov@yandex.ru>
2022-06-29 16:23:35 +0300

    Script without index_coloumn

--- main.py
+++ main.py
@@ -1,46 +1,53 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
+
 name_campagin = []
+import threading
 
 
 def generate_analytics_for_compaing(file_original_name: str):
-   df = pd.read_excel(f'{file_original_name}.xlsx')
-   print('Читаем файл')
-   for campaign_id in pd.unique(df['campaign_id']):
-       if campaign_id == 'None':
-           continue
-       name_campagin.append(campaign_id)
-       selection_by_campaign_id = df[df['campaign_id'] == campaign_id]
-
-       print(f'Создаём файл по выборке {campaign_id}')
-       name_new_file = f'Fromtech_{campaign_id}.xlsx'
-       selection_by_campaign_id.to_excel(name_new_file)
-       drop_coloumns(name_new_file,pd.read_excel(name_new_file))
+    df = pd.read_excel(f'{file_original_name}.xlsx')
+    print('Читаем файл')
+    for campaign_id in pd.unique(df['campaign_id']):
+        if campaign_id == 'None':
+            continue
+        name_campagin.append(campaign_id)
+        threads = threading.Thread(target=starting_threading, args=(campaign_id, df))
+        threads.start()
 
 
-def drop_coloumns(name_file,df):
+def starting_threading(campaign_id, df):
+    selection_by_campaign_id = df[df['campaign_id'] == campaign_id]
+    print(f'Создаём файл по выборке {campaign_id}')
+    name_new_file = f'Fromtech_{campaign_id}.xlsx'
+    selection_by_campaign_id.to_excel(name_new_file)
+    drop_coloumns(name_new_file, pd.read_excel(name_new_file))
+
+
+def drop_coloumns(name_file, df):
     with pd.ExcelWriter(name_file) as writer:
-        for name_coloumns in df.columns:
-            if name_coloumns != 'msisdn' and name_coloumns != 'CallDateTime' and name_coloumns != 'status_scheme':
-                df.drop(columns=name_coloumns, axis=1, inplace=True)
+        for name_columns in df.columns:
+            if name_columns != 'msisdn' and name_columns != 'CallDateTime' and name_columns != 'status_scheme':
+                df.drop(columns=name_columns, axis=1, inplace=True)
         print(f'Удалили столбцы из {name_file}')
-        new_df = df.replace(np.nan,'Недозвон',regex=True)
+        new_df = df.replace(np.nan, 'Недозвон', regex=True)
         print('Заменили пустые статусы на наедозвон')
         count_of_rows = new_df.count()
         count_of_rows_call_date = count_of_rows['CallDateTime']
         for i in range(int(count_of_rows_call_date)):
             if new_df.iloc[i]['CallDateTime'] == 'Недозвон':
-                new_df.loc[[i],'CallDateTime'] = new_df.iloc[i-1]['CallDateTime'][:-7]
+                new_df.loc[[i], 'CallDateTime'] = new_df.iloc[i - 1]['CallDateTime'][:-7]
             else:
                 new_df.loc[[i], 'CallDateTime'] = new_df.iloc[i]['CallDateTime'][:-7]
             if str(new_df.iloc[i]['msisdn'])[0] == '9':
                 new_df.loc[[i], 'msisdn'] = '7' + str(new_df.iloc[i]['msisdn'])
-        new_df.rename(columns={'msisdn': 'MSISDN','status_scheme': 'Результат звонка'}, inplace=True)
+        new_df.rename(columns={'msisdn': 'MSISDN', 'status_scheme': 'Результат звонка'}, inplace=True)
         print('Записываем в файл')
-        new_df.to_excel(writer,index=False)
+        new_df.to_excel(writer, index=False)
+
+
 time_now = datetime.now()
-generate_analytics_for_compaing('T2_SUPER_ONLINE_DISCOUNT_from_16_06_2022_00_00_00_to_29_06_2022')
+generate_analytics_for_compaing('Test')
 time_after = datetime.now()
 print(time_after - time_now)
-
