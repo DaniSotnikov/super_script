0391e46658ec1457b13d84b8ee5d544d8912e88d
alexanderspicin <spicin.2011@mail.ru>
2022-06-29 18:04:07 +0300

    merge tkinter with multiprocessing

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-import pandas as pd
-import numpy as np
-from datetime import datetime
-import threading
 from tkinter import *
 from tkinter import filedialog as fd
 import tkinter as tk
 import tkinter.font as tkFont
-from PIL import ImageTk, Image
-name_campagin = []
+import pandas as pd
+import numpy as np
 
+name_campagin = []
+from multiprocessing import Process
 
 
 def generate_analytics_for_compaing(file_original_name: str):
-    print('Yfxfkj aeyrwbb')
-    df = pd.read_excel(file_original_name)
+    print('Читаем файл')
+    df = pd.read_excel(f'{file_original_name}')
     print('Читаем файл')
     print('123')
+    procs = []
     for campaign_id in pd.unique(df['campaign_id']):
         if campaign_id == 'None':
             continue
         name_campagin.append(campaign_id)
-        threads = threading.Thread(target=starting_threading, args=(campaign_id, df))
-        threads.start()
+        proc = Process(target=starting_threading, args=(campaign_id, df))
+        procs.append(proc)
+        proc.start()
+    for proc in procs:
+        proc.join()
 
 
 def starting_threading(campaign_id, df):
@@ -29,15 +31,12 @@ def starting_threading(campaign_id, df):
     print(f'Создаём файл по выборке {campaign_id}')
     name_new_file = f'Fromtech_{campaign_id}.xlsx'
     selection_by_campaign_id.to_excel(name_new_file)
-    drop_coloumns(name_new_file, pd.read_excel(name_new_file))
-
-
-def drop_coloumns(name_file, df):
-    with pd.ExcelWriter(name_file) as writer:
+    df = pd.read_excel(name_new_file)
+    with pd.ExcelWriter(name_new_file) as writer:
         for name_columns in df.columns:
             if name_columns != 'msisdn' and name_columns != 'CallDateTime' and name_columns != 'status_scheme':
                 df.drop(columns=name_columns, axis=1, inplace=True)
-        print(f'Удалили столбцы из {name_file}')
+        print(f'Удалили столбцы из {name_new_file}')
         new_df = df.replace(np.nan, 'Недозвон', regex=True)
         print('Заменили пустые статусы на наедозвон')
         count_of_rows = new_df.count()
@@ -58,12 +57,18 @@ def insert_file():
     print(file_name)
     generate_analytics_for_compaing(file_name)
 
-root = Tk()
-root.title("Графическая программа на Python")
-a = root.geometry()
-fontStyle = tkFont.Font(family="Lucida Grande", size=20)
-b1 = Button(text="Сформировать отчёт", height=30,width=40, command=insert_file, bg='#ffc0cb',compound=tk.CENTER)
-b1.grid(row=500, column=100, ipadx=30, ipady=6, padx=600, pady=130)
 
-root.mainloop()
+if __name__ == '__main__':
+    root = Tk()
+    root.title("Графическая программа на Python")
+    a = root.geometry()
+    fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+    b1 = Button(text="Сформировать отчёт", height=30, width=40, command=insert_file, bg='#ffc0cb', compound=tk.CENTER)
+    b1.grid(row=500, column=100, ipadx=30, ipady=6, padx=600, pady=130)
+
+    root.mainloop()
+
+
+
+
 
